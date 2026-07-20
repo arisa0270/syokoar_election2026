f5e3d2dce3d7542beb04df2b8a9452e5feb35db3
arisa0270 <arisa0270@gmail.com>
2026-07-21 03:32:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+
+console.log("Three.js読み込み成功！");
+console.log("MindAR読み込み成功！");
+
+console.log(THREE);
+console.log(MindARThree);
