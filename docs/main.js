e71441a717953156ad8387ae673e8b01b08097e3
arisa0270 <arisa0270@gmail.com>
2026-07-28 03:40:17 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,36 @@ import { ARSystem } from "./systems/ar.js";
 
 const ar = new ARSystem();
 
+import { CharacterSystem } from "./systems/character.js";
+
 await ar.init();
 
+const character = new CharacterSystem(
+
+    ar.getAnchor()
+
+);
+
 await ar.start();
+
+character.createCharacter({
+
+    texture: "./assets/characters/cu_open.png",
+
+    width: 0.9,
+
+    height: 1.35,
+
+    x: 0,
+
+    y: 0.1,
+
+    z: 0
+
+});
+
+ar.addUpdate((delta)=>{
+
+    character.update(delta);
+
+});
