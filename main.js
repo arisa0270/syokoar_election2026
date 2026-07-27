9fb250c878238c648b0fba10431608912ec8e173
arisa0270 <arisa0270@gmail.com>
2026-07-28 03:27:48 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
-
-console.log("Three.js読み込み成功！");
-console.log("MindAR読み込み成功！");
-
-console.log(THREE);
-console.log(MindARThree);
